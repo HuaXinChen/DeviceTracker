04d061269ba1bc4159cbe680a4197456e89414c5
Victor Chen <huaxinchen@me.com>
2014-10-28 23:02:07 -0700

    Initial commit

---
+++ DeviceTracker/DeviceTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeviceTracker
+//
+//  Created by Victor Chen on 2014-10-28.
+//  Copyright (c) 2014 PNI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DeviceTracker/DeviceTracker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DeviceTracker
+//
+//  Created by Victor Chen on 2014-10-28.
+//  Copyright (c) 2014 PNI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
